2b8b6efb985093d859134ddbdecf0d2767334539
gaganbiswas <gaganbiswas.me1@gmail.com>
2021-08-05 03:50:04 +0530

    Initial Release

--- components/Loading.js
+++ components/Loading.js
@@ -1,10 +1,11 @@
+import Image from "next/image";
 import { Circle } from "better-react-spinkit";
 
 function Loading() {
   return (
     <center style={{ display: "grid", placeItems: "center", height: "100vh" }}>
       <div>
-        <img
+        <Image
           src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
           alt=""
           height={200}
